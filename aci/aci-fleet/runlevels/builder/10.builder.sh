ed97b2c2f080b68eb9f662760e4e0ca5bfb71a43
bob <bob@blablacar.com>
2017-11-08 13:23:48 +0100

    Build Fleet & path it instead of fetching the binaries.

--- aci/aci-fleet/runlevels/builder/10.builder.sh
+++ aci/aci-fleet/runlevels/builder/10.builder.sh
@@ -2,17 +2,41 @@
 
 . /dgr/bin/functions.sh
 isLevelEnabled "debug" && set -x
-set -ex
+set -e
 
 set -o pipefail
 
+export LC_ALL=C
+export GOROOT=/usr/local/go
+export GOPATH=/go
+export PATH=$PATH:/go/bin:/usr/local/go/bin
 
-curl -fL https://github.com/coreos/fleet/releases/download/v${ACI_VERSION}/fleet-v${ACI_VERSION}-linux-amd64.tar.gz -o fleet.tar.gz
-tar -xzvf fleet.tar.gz --strip-components=1
 
-upx -q fleetctl
-upx -t fleetctl
-upx -q fleetd
-upx -t fleetd
-mv -v fleetctl ${ROOTFS}/usr/bin/
-mv -v fleetd ${ROOTFS}/usr/bin/
+# Fetch sources
+WORK_DIR="${GOPATH}/src/github.com/coreos/fleet"
+go get -d github.com/coreos/fleet
+cd ${WORK_DIR}
+git checkout v${ACI_VERSION}
+# Apply custom patches
+PATCHES_DIR="${ACI_HOME}/patches"
+for patch in $(ls $PATCHES_DIR)
+do
+    echo "${PATCHES_DIR}/${patch}"
+    head -4 "${PATCHES_DIR}/${patch}"
+    patch -p1 < "${PATCHES_DIR}/${patch}" || {
+        echo >&2 "Unable to apply patch ${patch}"
+        exit 1
+    }
+    echo ""
+done
+export VERSION=$(git describe --dirty)
+# Build
+export GLDFLAGS="-X github.com/coreos/fleet/version.Version=${VERSION} -s -v -extldflags '-static'"
+CGO_ENABLED=0 go build -o bin/fleetd -a -installsuffix netgo -ldflags "${GLDFLAGS}"
+CGO_ENABLED=0 go build -o bin/fleetctl -a -installsuffix netgo -ldflags "${GLDFLAGS}" ./fleetctl
+upx -q bin/fleetctl
+upx -t bin/fleetctl
+upx -q bin/fleetd
+upx -t bin/fleetd
+mv -v bin/fleetctl ${ROOTFS}/usr/bin/
+mv -v bin/fleetd ${ROOTFS}/usr/bin/
